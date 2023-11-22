ae1b2d6425f1a12a582d0855a49297962d289e3b
Nazif Abdullah <ahmadnazif@gmail.com>
2023-11-23 00:05:52 +0800

    completes endpoints in SmsResource.java

--- src/main/java/io/anaz/SmsResource.java
+++ src/main/java/io/anaz/SmsResource.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 
 import io.quarkus.arc.lookup.LookupIfProperty.List;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("/sms")
 public class SmsResource {
     
+
+    @GET
+    @Path("count-all")
+    public int countAll(){
+        return 0;
+    }
+
+
     @GET
     public String get(){
         return "/";
@@ -18,5 +27,11 @@ public class SmsResource {
     @Path("/list-all")
     public ArrayList<String> listAll(){
         return new ArrayList<>();
+    }
+
+    @POST
+    @Path("/add")
+    public String add(){
+        return "Added";
     }
 }
