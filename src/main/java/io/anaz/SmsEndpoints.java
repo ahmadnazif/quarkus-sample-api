97fe18bbf209f69db1595fccc1ac7d5f47040432
Nazif Abdullah <ahmadnazif@gmail.com>
2023-11-23 11:23:04 +0800

    update dependency

--- src/main/java/io/anaz/SmsEndpoints.java
+++ src/main/java/io/anaz/SmsEndpoints.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 import io.anaz.models.PostResponse;
 import io.anaz.models.Sms;
+import io.anaz.models.SmsBase;
 import io.anaz.services.Db;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/sms")
 public class SmsEndpoints {
@@ -40,8 +43,8 @@ public class SmsEndpoints {
 
     @Path("/add")
     @POST
-    public PostResponse add() {
-        var resp = db.addData(null);
+    public PostResponse add(SmsBase sms) {
+        var resp = db.addData(sms);
         return resp;
     }
 
